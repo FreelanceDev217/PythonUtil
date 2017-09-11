97e95df84284e02153503c3d39b72590d2b14e2c
PCK <pck2016217@gmail.com>
2017-09-12 00:00:00 +0800

    Written 10 lines

--- work.py
+++ work.py
@@ -808,3 +808,13 @@ class webauto_base():
             self.log_error(str(e))
             return None
 
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
