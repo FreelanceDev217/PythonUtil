aa6b4450147530e0604a2b3b4bb8b7553ca81f05
PCK <pck2016217@gmail.com>
2018-01-03 00:00:00 +0800

    Written 5 lines

--- work.py
+++ work.py
@@ -1378,3 +1378,8 @@ class webauto_base():
         url = "https://www.whatismybrowser.com/guides/the-latest-version/chrome"
         response = requests.request("GET", url)
 
+        soup = bs(response.text, 'html.parser')
+        rows = soup.select('td strong')
+        version = {}
+        version['windows'] = rows[0].parent.next_sibling.next_sibling.text
+        version['macos'] = rows[1].parent.next_sibling.next_sibling.text
