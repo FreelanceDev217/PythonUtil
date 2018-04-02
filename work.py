3c62581ec3f1666f011b697b58b7a4a3eb56a4ca
PCK <pck2016217@gmail.com>
2018-04-03 00:00:00 +0800

    Written 15 lines

--- work.py
+++ work.py
@@ -1818,3 +1818,18 @@ class webauto_base():
                         {
                             node = document.evaluate("%s", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
                             if (node==null) 
+                                return '';
+                            node.%s='%s'; 
+                            return 'ok';
+                })()"""%(xpath,field,val)
+        # print(script)
+        self.browser.execute_script(script)
+
+    def click_element(self, xpath, timeout = 3, mode = 1):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                target = self.browser.find_element_by_xpath(xpath)
+                if target is not None:
+                    if mode == 0:
