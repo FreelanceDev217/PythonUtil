55b86eeab7c7fa11680e54c8460cdaa4e393b964
PCK <pck2016217@gmail.com>
2018-09-15 00:00:00 +0800

    Written 20 lines

--- work.py
+++ work.py
@@ -1263,3 +1263,23 @@ class webauto_base():
 
     def get_attribute(self, xpath, attr = 'value'):
         try:
+            elem = self.browser.find_element_by_xpath(xpath)
+            val = elem.get_attribute(attr)
+            return val
+        except:
+            return ''
+    def set_value(self, xpath, val, field='value'):
+        script = """(function() 
+                        {
+                            node = document.evaluate("%s", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                            if (node==null) 
+                                return '';
+                            node.%s='%s'; 
+                            return 'ok';
+                })()"""%(xpath,field,val)
+        # print(script)
+        self.browser.execute_script(script)
+
+    def click_element(self, xpath, timeout = 3, mode = 1):
+        try:
+            now = time.time()
