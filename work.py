4087e5b29470e22da9f96804fca7171e52eb39cd
PCK <pck2016217@gmail.com>
2019-01-11 00:00:00 +0800

    Written 20 lines

--- work.py
+++ work.py
@@ -3228,3 +3228,23 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.proxy import *
 from selenium.common.exceptions import TimeoutException
 
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.request as req
+
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+import api.states.python_anticaptcha as anticap
+ANTICAPTCHA_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+
+class webauto_base():
+    def __init__(self):
+        pass
+
+    # Start chrome browser for automation
+    def start_browser(self):
+        try:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--no-sandbox")
+            self.browser = webdriver.Chrome(executable_path='chromedriver', chrome_options = chrome_options)
