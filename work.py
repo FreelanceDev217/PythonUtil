e3059019fd4dd15ce5b905bc5275aa4d8d2e7384
PCK <pck2016217@gmail.com>
2019-05-08 00:00:00 +0800

    Written 10 lines

--- work.py
+++ work.py
@@ -3823,3 +3823,13 @@ class webauto_base():
     def refresh(self):
         self.browser.refresh()
 
+    # wait for <timeout> seconds
+    def delay_me(self, timeout = 3):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                pass
+            return True
+        except Exception as e:
+            return False
