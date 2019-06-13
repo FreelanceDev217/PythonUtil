280e8e119a72399d6bf318749a1c067076771db3
PCK <pck2016217@gmail.com>
2019-06-14 00:00:00 +0800

    Written 10 lines

--- work.py
+++ work.py
@@ -1063,3 +1063,13 @@ class webauto_base():
 
     def middle_click(self, xpath, timeout = 3):
         js = """
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
