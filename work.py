92c38bae635739213005928ea25d49d739e320c3
PCK <pck2016217@gmail.com>
2019-07-09 00:00:00 +0800

    Written 10 codes

--- work.py
+++ work.py
@@ -933,3 +933,13 @@ import requests
 import urllib.request as req
 
 import logging
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
