527a550e201ecc42f10f591ecc49729c808a3e6d
PCK <pck2016217@gmail.com>
2019-09-13 00:00:00 +0800

    Written 5 codes

--- work.py
+++ work.py
@@ -1268,3 +1268,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
