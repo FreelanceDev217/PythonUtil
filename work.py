a0b81f0b72ff566cf7cee7d49de2607066e5ef2d
PCK <pck2016217@gmail.com>
2019-10-03 00:00:00 +0800

    Written 25 codes

--- work.py
+++ work.py
@@ -1348,3 +1348,28 @@ from selenium import webdriver
 
 import time
 from selenium import webdriver
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.proxy import *
+from selenium.common.exceptions import TimeoutException
+
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.request as req
+
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+import api.states.python_anticaptcha as anticap
+ANTICAPTCHA_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+
