4e31f497f64bd3fd9b61355b8240ab99556e8001
PCK <pck2016217@gmail.com>
2019-10-12 00:00:00 +0800

    Written 5 codes

--- work.py
+++ work.py
@@ -3,8 +3,3 @@
 
 import time
 from selenium import webdriver
-from selenium.webdriver.firefox import options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
